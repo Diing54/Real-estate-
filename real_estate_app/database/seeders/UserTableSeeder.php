b9f0de3b2bc3743251c474eb350bd74171a5f66c
Michael-Njoroge <mikethecoder12@gmail.com>
2023-11-15 20:22:30 +0300

    complete admin,agent and user seeders

--- real_estate_app/database/seeders/UserTableSeeder.php
+++ real_estate_app/database/seeders/UserTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserTableSeeder extends Seeder
 {
@@ -12,6 +14,40 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users') -> insert([
+
+            //Admin
+
+            [
+                'name' => 'Administrator',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => Hash::make('password@123')
+            ],
+
+            //Agent
+
+            [
+                'name' => 'Agent',
+                'username' => 'agent',
+                'email' => 'agent@gmail.com',
+                'role' => 'agent',
+                'status' => 'active',
+                'password' => Hash::make('password@123')
+            ],
+
+            //User
+
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => Hash::make('password@123')
+            ],
+        ]);
     }
 }
