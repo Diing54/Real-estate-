1386d486526eaf0135839975a2775cfc593fd85e
Michael-Njoroge <mikethecoder12@gmail.com>
2023-11-15 20:44:06 +0300

    complete creating demo data through seeders

--- real_estate_app/database/seeders/UsersTableSeeder.php
+++ real_estate_app/database/seeders/UsersTableSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UserTableSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
