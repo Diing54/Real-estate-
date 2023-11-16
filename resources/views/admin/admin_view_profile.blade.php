de899018a4d028c82fdc79bb027b5d21df8b747c
Michael-Njoroge <mikethecoder12@gmail.com>
2023-11-16 22:30:40 +0300

    complete picture upload in update profile

--- resources/views/admin/admin_view_profile.blade.php
+++ resources/views/admin/admin_view_profile.blade.php
@@ -2,6 +2,7 @@
 
 @section('admin')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <div class="page-content">
 
  
@@ -55,30 +56,47 @@
     <div class="card">
               <div class="card-body">
 
-								<h6 class="card-title">Basic Form</h6>
+								<h6 class="card-title">Update Admin Profile</h6>
 
 								<form class="forms-sample">
 									<div class="mb-3">
 										<label for="exampleInputUsername1" class="form-label">Username</label>
-										<input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Username">
+										<input type="text" class="form-control" name="username" id="username" value="{{$profileData->username}}" autocomplete="off">
+									</div>
+                                    <div class="mb-3">
+										<label for="name" class="form-label">Name</label>
+										<input type="text" class="form-control" id="name" name="name" value="{{$profileData->name}}" >
 									</div>
 									<div class="mb-3">
-										<label for="exampleInputEmail1" class="form-label">Email address</label>
-										<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+										<label for="email" class="form-label">Email address</label>
+										<input type="email" class="form-control" id="exampleInputEmail1" name="email" value="{{$profileData->email}}"  >
 									</div>
-									<div class="mb-3">
-										<label for="exampleInputPassword1" class="form-label">Password</label>
-										<input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password">
+                                    <div class="mb-3">
+										<label for="phone" class="form-label">Phone</label>
+										<input type="text" class="form-control" id="phone" name="phone" value="{{$profileData->phone}}"  >
 									</div>
+                                    <div class="mb-3">
+										<label for="address" class="form-label">Address</label>
+										<input type="text" class="form-control" id="address" name="address" value="{{$profileData->address}}"  >
+									</div>
+                                    <div class="mb-3">
+										<label for="photo" class="form-label">Photo</label>
+										<input type="file" class="form-control" id="image" name="photo">
+									</div>
+
+                                    <div class="mb-3">
+										<label for="photo" class="form-label"> </label>
+                                        <img id="showImage" class="wd-80 rounded-circle" src="{{ (!empty($profileData -> photo)) ? url('upload/admin_images/'.$profileData -> photo) : url('upload/no_image.jpg')}}" alt="profile">
+									</div>
+		 
 									<div class="form-check mb-4">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
 										<label class="form-check-label" for="exampleCheck1">
 											Remember me
 										</label>
 									</div>
-									<button type="submit" class="btn btn-primary me-2">Submit</button>
-									<button class="btn btn-secondary">Cancel</button>
-								</form>
+									<button type="submit" class="btn btn-primary me-2">Save Changes</button>
+ 								</form>
 
               </div>
             </div>
@@ -91,5 +109,18 @@
 </div>
 
     </div>
+
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#image').change(function(e){
+                var reader = new FileReader();
+                reader.onload = function(e){
+                    $('#showImage').attr('src',e.target.result);
+                } 
+                reader.readAsDataURL(e.target.files['0']);
+            });
+        });
+    </script>
 
 @endsection
