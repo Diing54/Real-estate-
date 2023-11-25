b7dbb31ea9a4e24588b34039798af67920f8d43a
Michael-Njoroge <mikethecoder12@gmail.com>
2023-11-25 17:42:44 +0300

    permissions to access amenities

--- resources/views/backend/amenity/all_amenity.blade.php
+++ resources/views/backend/amenity/all_amenity.blade.php
@@ -5,9 +5,11 @@
 <div class="page-content">
 
 				<nav class="page-breadcrumb">
+        @if(Auth::user()->can('add.amenity'))
 					<ol class="breadcrumb">
                     <a href="{{route('add.amenity')}}" class="btn btn-inverse-info">Add New Amenity</a>
 					</ol>
+          @endif
 				</nav>
 
 				<div class="row">
@@ -31,8 +33,12 @@
                         <td>{{$key+1}}</td>
                         <td>{{$item->amenity_name}}</td>
                          <td> 
+                         @if(Auth::user()->can('edit.amenity'))
                             <a href="{{route('edit.amenity', $item->id)}}" class="btn btn-inverse-warning">Edit</a>
+                         @endif
+                         @if(Auth::user()->can('delete.amenity'))  
                             <a href="{{route('delete.amenity', $item->id)}}" class="btn btn-inverse-danger" id="delete">Delete</a>
+                        @endif
                         </td>
                       </tr>
                       @endforeach
